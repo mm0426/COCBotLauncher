4e89494367c0357cb92719cd090239062f7ce0fe
Michael Matthews <mm042676@gmail.com>
2018-02-09 08:30:23 -0500

    moved all variables to xml

--- cocbotlauncher.ps1
+++ cocbotlauncher.ps1
@@ -1,13 +1,14 @@
 #coclauncher - powershell script to launch clash of clans bot
 
-$mybotPath = "C:\MyBot-MBR_v7.4.3\"
-
 [xml]$botdata = get-content ./botdata.xml
 
 Write-Host "====== Bots ====="
 foreach ($bot in $botdata.bots.bot) { Write-Host $bot.key $bot.profile }
+[int]$choice = read-host "Enter the number for the bot you wish to launch"
 
-#$botToLaunch = "wedey"
-#$memuInstance = "MEmu"
-#$command = $mybotPath + "mybot.run.exe " + $botToLaunch + " MEmu " + $memuInstance
-#invoke-expression $command
+$mybotPath = $botdata.bots.mybotPath
+$botToLaunch = $botdata.bots.bot[$choice-1].profile
+$memuInstance = $botdata.bots.bot[$choice-1].memuInstance
+$command = $mybotPath + "mybot.run.exe " + $botToLaunch + " MEmu " + $memuInstance
+invoke-expression $command
+
